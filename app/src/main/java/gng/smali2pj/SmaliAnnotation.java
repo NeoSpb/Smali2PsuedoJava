80806073e85f892ef96b58110983b593e8ecc22a
NeoSpb <neospb86@gmail.com>
2021-03-29 23:21:26 +0300

    * Reformat code

--- app/src/main/java/gng/smali2pj/SmaliAnnotation.java
+++ app/src/main/java/gng/smali2pj/SmaliAnnotation.java
@@ -5,42 +5,42 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SmaliAnnotation {
-	String SYSTEM;
-	String TYPE;
-	HashMap<String, String> CONTENTS;
-	
-	public SmaliAnnotation(ArrayList<String> annoChunk){
-		CONTENTS = new HashMap<String, String>();
-		
-		for (int i=0; i<annoChunk.size(); i++){
-			String line = annoChunk.get(i);
-			if (line.startsWith(".annotation")){
-				line = Utils.removeFirstWord(line);
-				String[] parts = line.split(" ");
-				this.SYSTEM = parts[0];
-				this.TYPE = Utils.determineType(parts[1]);
-			}
-			else if (line.startsWith(".end annotation")){} // do nothing
-			else {				
-				line = line.trim();
-				String key = line.substring(0,line.indexOf(" = "));
-				String value = line.substring(line.indexOf(" = ") + 3);
-				if (value.startsWith("{")){
-					for (int j=i+1; j<annoChunk.size(); j++){
-						String nLine = annoChunk.get(j).trim();
-						value += nLine;
-						if (nLine.startsWith("}")){
-							i = j;
-							break;
-						}
-					}
-				}
-				CONTENTS.put(key, value);
-			}
-		}
-	}
-	
-	public void writeToFile(BufferedWriter writer){
-		
-	}
+    String SYSTEM;
+    String TYPE;
+    HashMap<String, String> CONTENTS;
+
+    public SmaliAnnotation(ArrayList<String> annoChunk) {
+        CONTENTS = new HashMap<>();
+
+        for (int i = 0; i < annoChunk.size(); i++) {
+            String line = annoChunk.get(i);
+            if (line.startsWith(".annotation")) {
+                line = Utils.removeFirstWord(line);
+                String[] parts = line.split(" ");
+                this.SYSTEM = parts[0];
+                this.TYPE = Utils.determineType(parts[1]);
+            } else if (line.startsWith(".end annotation")) {
+            } // do nothing
+            else {
+                line = line.trim();
+                String key = line.substring(0, line.indexOf(" = "));
+                StringBuilder value = new StringBuilder(line.substring(line.indexOf(" = ") + 3));
+                if (value.toString().startsWith("{")) {
+                    for (int j = i + 1; j < annoChunk.size(); j++) {
+                        String nLine = annoChunk.get(j).trim();
+                        value.append(nLine);
+                        if (nLine.startsWith("}")) {
+                            i = j;
+                            break;
+                        }
+                    }
+                }
+                CONTENTS.put(key, value.toString());
+            }
+        }
+    }
+
+    public void writeToFile(BufferedWriter writer) {
+
+    }
 }
--- app/src/main/java/gng/smali2pj/Utils.java
+++ app/src/main/java/gng/smali2pj/Utils.java
@@ -5,171 +5,170 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Utils {
-	public static int PORTION_HEADER = 0;
-	public static int PORTION_INSTANCE_FIELDS = 1;
-	public static int PORTION_STATIC_FIELDS = 2;
-	public static int PORTION_DIRECT_METHODS = 3;
-	public static int PORTION_VIRTUAL_METHODS = 4;
-	static String MARKER_INSTANCE_FIELDS = "# instance fields";
-	static String MARKER_STATIC_FIELDS = "# static fields";
-	static String MARKER_DIRECT_METHODS = "# direct methods";
-	static String MARKER_VIRTUAL_METHODS = "# virtual methods";
-	
-	public static boolean SHOW_DEST_TYPE = false;
-	public static boolean REPLACE_VAR_NAMES = false;
-	
-	public static int PADDING_INFRONT = 0;
-	public static int PADDING_BEHIND = 1;
-	
-	public static ArrayList<String> processParamTypes(String line){
-		ArrayList<String> params = new ArrayList<String>();
-		String curChar = "";
-		String curElem = "";
-		String arr = "";
-		for (int i=0; i<line.length(); i++){
-			curChar = line.substring(i, i+1);
-			if(isArrayType(curChar)) {
-				arr += "[";
-				continue;
-			}
-			
-			if (curElem.isEmpty()){
-				if (isBasicType(curChar)) {
-					params.add(arr + curChar);
-					arr = "";
-				}
-				else curElem += curChar;				
-			}
-			else{
-				if (curChar.equals(";")){
-					params.add(arr + curElem);
-					curElem = "";
-					arr = "";
-				}
-				else curElem += curChar;
-			}
-		}
-		
-		for (int i=0; i<params.size(); i++){
-			params.set(i, determineType(params.get(i)));
-		}
-		
-		return params;
-	}
-	
-	private static boolean isBasicType(String curChar){
-		if (curChar.equals("V")) return true;
-		else if (curChar.equals("Z")) return true;
-		else if (curChar.equals("B")) return true;
-		else if (curChar.equals("S")) return true;
-		else if (curChar.equals("C")) return true;
-		else if (curChar.equals("I")) return true;
-		else if (curChar.equals("F")) return true;
-		else if (curChar.equals("J")) return true;
-		else if (curChar.equals("D")) return true;
-		else return false;
-	}
-	
-	private static boolean isArrayType(String curChar){
-		if (curChar.equals("[")) return true;
-		else return false;
-	}
-	
-	public static String determineType(String type){
-		type = type.trim();
-		if (type.isEmpty()) return "";
-		
-		String arrAppend = "";
-		for (int i=0; i<type.length(); i++){
-			if (type.substring(i, i+1).equals("[")){
-				arrAppend += "[]";
-				type = type.substring(i+1);
-			}
-			else break;
-		}
-		
-		if (type.equals("V")) return "void";
-		else if (type.equals("Z")) return "boolean"+arrAppend;
-		else if (type.equals("B")) return "byte"+arrAppend;
-		else if (type.equals("S")) return "short"+arrAppend;
-		else if (type.equals("C")) return "char"+arrAppend;
-		else if (type.equals("I")) return "int"+arrAppend;
-		else if (type.equals("F")) return "float"+arrAppend;
-		else if (type.equals("J")) return "long"+arrAppend;
-		else if (type.equals("D")) return "double"+arrAppend;
-		else {
-			// Objects			
-			if (type.startsWith("L")){
-				// Remove last char if it is ;
-				if (type.substring(type.length()-1).equals(";")) return type.substring(1, type.length()-1) + arrAppend;
-				else return type.substring(1) + arrAppend;
-			}
-			else return null;
-		}
-	}
-	
-	public static String removeFirstWord(String line){
-		return line.substring(line.indexOf(" ")+1);
-	}
-	
-	public static void writeLine(BufferedWriter writer, String line, int numTabs) throws IOException{				
-		write(writer, line + "\n", numTabs);
-	}
-	
-	public static void write(BufferedWriter writer, String line, int numTabs) throws IOException{
-		String output = line;
-		for (int i=0; i<numTabs; i++) output = "\t" + output;
-		writer.write(output);
-	}
-	
-	public static int isNextPortion(String line){
-		if 		(line.startsWith(MARKER_INSTANCE_FIELDS)) 	return PORTION_INSTANCE_FIELDS;
-		else if (line.startsWith(MARKER_STATIC_FIELDS)) 	return PORTION_STATIC_FIELDS;
-		else if	(line.startsWith(MARKER_DIRECT_METHODS))	return PORTION_DIRECT_METHODS;
-		else if (line.startsWith(MARKER_VIRTUAL_METHODS))	return PORTION_VIRTUAL_METHODS;
-		else return -1;
-	}
-	
-	public static String removeComments(String line){
-		if (line.indexOf("#") >= 0){
-			line = line.substring(0, line.indexOf("#"));
-		}
-		return line;
-	}
-	
-	public static String padToWide(String input, int direction){
-		if (input.startsWith("0x")) input = input.substring(2);
-		if (direction == Utils.PADDING_INFRONT){
-			// Pad Infront
-			while (input.length() < 16) input = "0" + input;
-		}
-		else if (direction == Utils.PADDING_BEHIND){
-			// Pad Behind
-			while (input.length() < 16) input = input + "0";
-		}
-		else input = "[ERROR] Unknown Wide Padding Direction";
-		return "0x"+input;
-	}
-	
-	public static String padToNormal(String input, int direction){
-		if (input.startsWith("0x")) input = input.substring(2);
-		if (direction == Utils.PADDING_INFRONT){
-			// Pad Infront
-			while (input.length() < 8) input = "0" + input;
-		}
-		else if (direction == Utils.PADDING_BEHIND){
-			// Pad Behind
-			while (input.length() < 8) input = input + "0";
-		}
-		else input = "[ERROR] Unknown Normal Padding Direction";
-		return "0x"+input;
-	}
-	
-	public static String splitWideValue(String widevalue){
-		if (widevalue.startsWith("0x")) widevalue = widevalue.substring(2);
-		widevalue = widevalue.substring(0,8) + " " + widevalue.substring(8);
-		return "0x"+widevalue;
-	}
+    public static int PORTION_HEADER = 0;
+    public static int PORTION_INSTANCE_FIELDS = 1;
+    public static int PORTION_STATIC_FIELDS = 2;
+    public static int PORTION_DIRECT_METHODS = 3;
+    public static int PORTION_VIRTUAL_METHODS = 4;
+    static String MARKER_INSTANCE_FIELDS = "# instance fields";
+    static String MARKER_STATIC_FIELDS = "# static fields";
+    static String MARKER_DIRECT_METHODS = "# direct methods";
+    static String MARKER_VIRTUAL_METHODS = "# virtual methods";
+
+    public static boolean SHOW_DEST_TYPE = false;
+    public static boolean REPLACE_VAR_NAMES = false;
+
+    public static int PADDING_INFRONT = 0;
+    public static int PADDING_BEHIND = 1;
+
+    public static ArrayList<String> processParamTypes(String line) {
+        ArrayList<String> params = new ArrayList<>();
+        String curChar;
+        StringBuilder curElem = new StringBuilder();
+        StringBuilder arr = new StringBuilder();
+        for (int i = 0; i < line.length(); i++) {
+            curChar = line.substring(i, i + 1);
+            if (isArrayType(curChar)) {
+                arr.append("[");
+                continue;
+            }
+
+            if (curElem.length() == 0) {
+                if (isBasicType(curChar)) {
+                    params.add(arr + curChar);
+                    arr = new StringBuilder();
+                } else curElem.append(curChar);
+            } else {
+                if (curChar.equals(";")) {
+                    params.add(arr.toString() + curElem);
+                    curElem = new StringBuilder();
+                    arr = new StringBuilder();
+                } else curElem.append(curChar);
+            }
+        }
+
+        for (int i = 0; i < params.size(); i++) {
+            params.set(i, determineType(params.get(i)));
+        }
+
+        return params;
+    }
+
+    private static boolean isBasicType(String curChar) {
+        if (curChar.equals("V")) return true;
+        else if (curChar.equals("Z")) return true;
+        else if (curChar.equals("B")) return true;
+        else if (curChar.equals("S")) return true;
+        else if (curChar.equals("C")) return true;
+        else if (curChar.equals("I")) return true;
+        else if (curChar.equals("F")) return true;
+        else if (curChar.equals("J")) return true;
+        else if (curChar.equals("D")) return true;
+        else return false;
+    }
+
+    private static boolean isArrayType(String curChar) {
+        return curChar.equals("[");
+    }
+
+    public static String determineType(String type) {
+        type = type.trim();
+        if (type.isEmpty()) return "";
+
+        String arrAppend = "";
+        for (int i = 0; i < type.length(); i++) {
+            if (type.charAt(i) == '[') {
+                arrAppend += "[]";
+                type = type.substring(i + 1);
+            } else break;
+        }
+
+        if (type.equals("V")) return "void";
+        else if (type.equals("Z")) return "boolean" + arrAppend;
+        else if (type.equals("B")) return "byte" + arrAppend;
+        else if (type.equals("S")) return "short" + arrAppend;
+        else if (type.equals("C")) return "char" + arrAppend;
+        else if (type.equals("I")) return "int" + arrAppend;
+        else if (type.equals("F")) return "float" + arrAppend;
+        else if (type.equals("J")) return "long" + arrAppend;
+        else if (type.equals("D")) return "double" + arrAppend;
+        else {
+            // Objects
+            if (type.startsWith("L")) {
+                // Remove last char if it is ;
+                if (type.substring(type.length() - 1).equals(";"))
+                    return type.substring(1, type.length() - 1) + arrAppend;
+                else return type.substring(1) + arrAppend;
+            } else return null;
+        }
+    }
+
+    public static String removeFirstWord(String line) {
+        return line.substring(line.indexOf(" ") + 1);
+    }
+
+    public static void writeLine(BufferedWriter writer, String line, int numTabs) throws IOException {
+        write(writer, line + "\n", numTabs);
+    }
+
+    public static void write(BufferedWriter writer, String line, int numTabs) throws IOException {
+        StringBuilder output = new StringBuilder(line);
+        for (int i = 0; i < numTabs; i++) output.insert(0, "\t");
+        writer.write(output.toString());
+    }
+
+    public static int isNextPortion(String line) {
+        if (line.startsWith(MARKER_INSTANCE_FIELDS)) return PORTION_INSTANCE_FIELDS;
+        else if (line.startsWith(MARKER_STATIC_FIELDS)) return PORTION_STATIC_FIELDS;
+        else if (line.startsWith(MARKER_DIRECT_METHODS)) return PORTION_DIRECT_METHODS;
+        else if (line.startsWith(MARKER_VIRTUAL_METHODS)) return PORTION_VIRTUAL_METHODS;
+        else return -1;
+    }
+
+    public static String removeComments(String line) {
+        if (line.contains("#")) {
+            line = line.substring(0, line.indexOf("#"));
+        }
+        return line;
+    }
+
+    public static String padToWide(String input, int direction) {
+        if (input.startsWith("0x")) input = input.substring(2);
+        if (direction == Utils.PADDING_INFRONT) {
+            // Pad Infront
+            StringBuilder inputBuilder = new StringBuilder(input);
+            while (inputBuilder.length() < 16) inputBuilder.insert(0, "0");
+            input = inputBuilder.toString();
+        } else if (direction == Utils.PADDING_BEHIND) {
+            // Pad Behind
+            StringBuilder inputBuilder = new StringBuilder(input);
+            while (inputBuilder.length() < 16) inputBuilder.append("0");
+            input = inputBuilder.toString();
+        } else input = "[ERROR] Unknown Wide Padding Direction";
+        return "0x" + input;
+    }
+
+    public static String padToNormal(String input, int direction) {
+        if (input.startsWith("0x")) input = input.substring(2);
+        if (direction == Utils.PADDING_INFRONT) {
+            // Pad Infront
+            StringBuilder inputBuilder = new StringBuilder(input);
+            while (inputBuilder.length() < 8) inputBuilder.insert(0, "0");
+            input = inputBuilder.toString();
+        } else if (direction == Utils.PADDING_BEHIND) {
+            // Pad Behind
+            StringBuilder inputBuilder = new StringBuilder(input);
+            while (inputBuilder.length() < 8) inputBuilder.append("0");
+            input = inputBuilder.toString();
+        } else input = "[ERROR] Unknown Normal Padding Direction";
+        return "0x" + input;
+    }
+
+    public static String splitWideValue(String widevalue) {
+        if (widevalue.startsWith("0x")) widevalue = widevalue.substring(2);
+        widevalue = widevalue.substring(0, 8) + " " + widevalue.substring(8);
+        return "0x" + widevalue;
+    }
 	
 	/*
 	private String s2pj_IfLessThan(String line){
